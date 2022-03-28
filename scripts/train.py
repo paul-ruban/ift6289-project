5aef88d650b5ed7f9e88adfdf0cef269a0fa457c
paul-ruban <paulrubanbj@gmail.com>
2022-03-27 23:37:35 -0400

    Add script, config files

---
+++ scripts/train.py
@@ -0,0 +1,63 @@
+import json
+import argparse
+from functools import partial
+from src.config import TrainConfig
+from src.trainer import SQUADTrainer
+from src.data import preprocess_function
+from datasets import load_dataset
+from transformers import (
+    TrainingArguments, 
+    AutoModelForQuestionAnswering,
+    AutoTokenizer
+)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c',"--config", help="Config file.", required=True)
+    args = parser.parse_args()
+
+    train_config = TrainConfig.from_json(args.model)
+
+    tokenizer = AutoTokenizer.from_pretrained(train_config.model)
+    model = AutoModelForQuestionAnswering.from_pretrained(train_config.model)
+    
+    teacher_model = None
+    if train_config.teacher_model is None:
+        teacher_model = AutoModelForQuestionAnswering.from_pretrained(train_config.teacher_model)
+    
+    dataset = load_dataset(train_config.data_path)
+
+    # Preprocess dataset
+    dataset = dataset.map(
+        partial(preprocess_function, tokenizer=tokenizer),
+        batched=True,
+        remove_columns=dataset["train"].column_names
+    )
+
+    training_args = TrainingArguments(
+        output_dir=train_config.output_dir,
+        evaluation_strategy=train_config.evaluation_strategy,
+        logging_steps=train_config.logging_steps,
+        learning_rate=train_config.learning_rate,
+        per_device_train_batch_size=train_config.per_device_train_batch_size,
+        per_device_eval_batch_size=train_config.per_device_eval_batch_size,
+        num_train_epochs=train_config.num_train_epochs,
+        weight_decay=train_config.weight_decay,
+        disable_tqdm=train_config.disable_tqdm
+    )
+
+    trainer = SQUADTrainer(
+        model=train_config.model,
+        teacher_model=teacher_model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"],
+        tokenizer=tokenizer
+    )
+
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/config.py
@@ -0,0 +1,38 @@
+import json
+
+
+class TrainConfig:
+    def __init__(
+        self,
+        model=None,
+        teacher_model=None,
+        data_path=None,
+        output_dir="../logs",
+        evaluation_strategy="steps",
+        logging_steps=1000,
+        learning_rate=5e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=10,
+        weight_decay=0.01,
+        disable_tqdm=True
+    ):
+        self.model = model
+        self.teacher_model = teacher_model
+        self.data_path = data_path
+        self.output_dir = output_dir
+        self.evaluation_strategy = evaluation_strategy
+        self.logging_steps = logging_steps
+        self.learning_rate = learning_rate
+        self.per_device_train_batch_size = per_device_train_batch_size
+        self.per_device_eval_batch_size = per_device_eval_batch_size
+        self.num_train_epochs = num_train_epochs
+        self.weight_decay = weight_decay
+        self.disable_tqdm = disable_tqdm
+    
+    @classmethod
+    def from_json(cls, path):
+        with open(path) as f:
+            config_dict = json.load(f)
+        
+        return cls(**config_dict)
