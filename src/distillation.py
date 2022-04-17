43cb3e57c55976cefef7f84511215e80a447a5dc
paul-ruban <paulrubanbj@gmail.com>
2022-04-17 18:51:34 -0400

    remove reduction

--- src/distillation.py
+++ src/distillation.py
@@ -48,7 +48,7 @@ class FactorTransfer(torch.nn.Module):
 		Returns:
 			torch.Tensor: The loss for the SoftTarget distillation method.
 		"""
-		loss = F.l1_loss(self.normalize(input), self.normalize(target), reduction="mean")
+		loss = F.l1_loss(self.normalize(input), self.normalize(target))
 		return loss
 
 	def normalize(self, input):
@@ -93,7 +93,7 @@ class CorrelationCongruence(torch.nn.Module):
 		corr_matrix_input = self.correlation_matrix(input)
 		corr_matrix_target = self.correlation_matrix(target)
 
-		loss = F.mse_loss(corr_matrix_input, corr_matrix_target, reduction="mean")
+		loss = F.mse_loss(corr_matrix_input, corr_matrix_target)
 
 		return loss
 
