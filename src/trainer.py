c4d1dd74865d40992024c27e919d06425c1a7a11
paul-ruban <paulrubanbj@gmail.com>
2022-04-18 19:55:24 -0400

    Fix l0

--- src/trainer.py
+++ src/trainer.py
@@ -755,7 +755,6 @@ class SQUADTrainer(Trainer):
             l0_loss = 0.001 * outputs["attentions"]
             loss_dict["l0_loss"] = l0_loss
             loss_total += l0_loss
-
       
         loss_dict["loss_qa"] = loss_qa
         loss_dict["loss_total"] = loss_total
